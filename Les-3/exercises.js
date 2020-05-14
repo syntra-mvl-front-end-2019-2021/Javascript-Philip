3c7bbcdc90cc8e1a7a69ded4f9f7c6b2770f9e5f
philip franssen <philipfranssen@hotmail.com>
2020-05-14 16:51:46 +0200

    exercises

--- Les-3/exercises.js
+++ Les-3/exercises.js
@@ -4,8 +4,8 @@
 //         let largestVal = Math.max(...anArray);
 //         return largestVal;    
 // }
-let array = [1,2,3]
-      function  getLargestItemInArray() {
+// let array = [1,2,3]
+      function  getLargestItemInArray(array) {
         let largestVal = array[0];
         for ( let i = 0; i < array.length; i++){
               if( array[i] > largestVal   ) {
@@ -17,18 +17,18 @@ let array = [1,2,3]
 
 
 // Reverse order of array, e.g. [1,2,3] → [3,2,1]
-let anotherArray = [1, 2, 3];
+// let anotherArray = [1, 2, 3];
 
-function reverseArray() {
+function reverseArray(anotherArray) {
   let reversed = anotherArray.reverse();
   return reversed;
 }
 // console.log(reverseArray());
 
 // Make a sum of all elements, e.g. [1,2,3] → 6
-let thirdArray = [1, 3, 2];
+// let thirdArray = [1, 3, 2];
 
-function arraySum() {
+function arraySum(thirdArray) {
   let sum = 0;
   for (let i=0; i < thirdArray.length; i++) {
       sum = sum + thirdArray[i];
@@ -38,25 +38,55 @@ function arraySum() {
 // console.log(arraySum());
 
 // Make a string of the first letter of each element of array, e.g. ['Dog', 'cat', 'snake'] → 'Dcs'
-let anArray = ['Dog', 'cat', 'snake'];
-function stringOfFirstLetters() {
+// let anArray = ['Dog', 'cat', 'snake'];
+function stringOfFirstLetters(anArray) {
 
-  let x = anArray[0].substring(0,1);
+  let firstLetter = anArray[0].substring(0,1);
   for ( let i = 1; i < anArray.length; i++)   
 {
- x += anArray[i].substring(0,1)
+ firstLetter += anArray[i].substring(0,1)
 
 }
-return x
+return firstLetter
 }
 
 // Combines two arrays by alternatingly taking elements, e.g. ['a','b','c'], [1,2,3] → ['a',1,'b',2,'c',3].
-function combineArrays(arrayOne, arrayTwo) {
+// let array1 = ['a','b','c'];
+// let array2 = [1,2,3];
 
-}
+
+function combineArrays(array1, array2) {
+
+  let comArr = [];
+  let length = array1.length + array2.length;
+  
+          if( array1.length < array2.length ) {
+            for (let i = 0; i < array2.length; i++ ) {
+    comArr.push( array1[i] , array2[i] )
+  }
+          }
+  
+          else if( array1.length > array2.length) {
+            for (let i = 0; i < array1.length; i++ ) {
+    comArr.push( array1[i] , array2[i] )
+  }
+          }
+    else {
+        for (let i = 0; i < array1.length; i++ ) {
+    comArr.push( array1[i] , array2[i] )
+  }
+    }
+  
+  return comArr;
+  }
 
 // Takes a number and returns a list of its digits. e.g. 2342 → [2,3,4,2]
+// let numbers = 2342;
+
 function numberToDigitArray(aNumber) {
+
+let numArr = Array.from(aNumber.toString()).map(Number);
+return numArr;
 
 }
 
