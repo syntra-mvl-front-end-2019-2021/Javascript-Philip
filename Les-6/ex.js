16bdad4130646cae2d37a67ddf86971cb64242a2
philip franssen <philipfranssen@hotmail.com>
2020-05-29 13:42:42 +0200

    Les-6 ex add input

--- Les-6/ex.js
+++ Les-6/ex.js
@@ -4,6 +4,7 @@ let sumButton = document.getElementById('sum-submit-btn');
 let sum = document.getElementById('sum-result-container');
 let productButton = document.getElementById('product-submit-btn');
 let product = document.getElementById('product-result-container');
+let addInputButton = document.getElementById('add-input-btn');
 
 // console.log(input1);
 // console.log(input2);
@@ -25,4 +26,39 @@ function printProduct (event) {
 
     product.innerText = parseInt(input1.value) * parseInt(input2.value);
     
+}
+
+
+addInputButton.addEventListener('click', addInput);
+
+
+function addInput (event) {
+
+
+
+let newFieldDiv = document.createElement('div');
+let newControlInputDiv = document.createElement('div');
+let newInput = document.createElement('input');
+let newControlDeleteDiv = document.createElement('div');
+let newADelete = document.createElement('a');
+let inputContainer = document.getElementById('input-container')
+
+newADelete.innerText = 'X';
+
+inputContainer.appendChild(newFieldDiv);
+newFieldDiv.appendChild(newControlInputDiv);
+newControlInputDiv.appendChild(newInput);
+newFieldDiv.appendChild(newControlDeleteDiv);
+newControlDeleteDiv.appendChild(newADelete);
+
+newFieldDiv.setAttribute('class', 'field has-addons number-input-group');
+newControlInputDiv.setAttribute('class', 'control');
+newInput.setAttribute('id', 'input-3');
+newInput.setAttribute('class', 'input number-input');
+newInput.setAttribute('type', 'number');
+newInput.setAttribute('step', '1');
+newInput.setAttribute('value', '0');
+newControlDeleteDiv.setAttribute('class', 'control');
+newADelete.setAttribute('class', 'button is-danger delete-number-input');
+
 }
