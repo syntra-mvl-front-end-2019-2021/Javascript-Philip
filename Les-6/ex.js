271beecb39c1f2dec230c37ec533183f42706c1f
philip franssen <philipfranssen@hotmail.com>
2020-06-05 15:30:19 +0200

    Les-6 ex

--- Les-6/ex.js
+++ Les-6/ex.js
@@ -1,94 +1,106 @@
-let input1 = document.getElementById('input-1');
-let input2 = document.getElementById('input-2');
-let sumButton = document.getElementById('sum-submit-btn');
-let sum = document.getElementById('sum-result-container');
-let productButton = document.getElementById('product-submit-btn');
-let product = document.getElementById('product-result-container');
-let addInputButton = document.getElementById('add-input-btn');
+const sumSubmitBtn = document.querySelector('#sum-submit-btn');
+const sumResultContainer = document.querySelector('#sum-result-container');
+const productSubmitBtn = document.querySelector('#product-submit-btn');
+const productResultContainer = document.querySelector('#product-result-container');
+const numberInputContainer = document.querySelector('#input-container');
+const addInputBtn = document.querySelector('#add-input-btn');
+const numberCountContainer = document.querySelector('#number-count-container');
 
+function sumNumbers() {
+  // return the sum value of all the numbers
+  const numberInputs = document.querySelectorAll('.number-input');
+//   const numberInputs = document.getElementsByClassName('number-input');
 
-// console.log(input1);
-// console.log(input2);
-// console.log(sumButton);
-// console.log(sum);
+  let totalSum = 0;
 
-sumButton.addEventListener('click', printSum);
+  for (let numberInput of numberInputs) {
+    totalSum += parseInt(numberInput.value);
+  }
+  
+//   for (let i = 0; i < numberInputs.length; i++) {
+//     totalSum += parseInt(numberInputs[i].value);
+//   }
 
-function printSum (event) {
-
-    sum.innerText = parseInt(input1.value) + parseInt(input2.value);
-    
+  return totalSum;
 }
 
-
-productButton.addEventListener('click', printProduct);
-
-function printProduct (event) {
-
-    product.innerText = parseInt(input1.value) * parseInt(input2.value);
-    
+function printSum() {
+  // print the sum value of all the numbers
+  sumResultContainer.textContent = sumNumbers();
 }
 
-
-
-
-addInputButton.addEventListener('click', addInput);
-
-
-function addInput (event) {
-
-    let number = 2;
-    let count = document.getElementById('number-count-container');
-    count.innerText = ++number;
-
-let newFieldDiv = document.createElement('div');
-let newControlInputDiv = document.createElement('div');
-let newInput = document.createElement('input');
-let newControlDeleteDiv = document.createElement('div');
-let newADelete = document.createElement('a');
-let inputContainer = document.getElementById('input-container')
-
-newADelete.innerText = 'X';
-
-
-inputContainer.appendChild(newFieldDiv);
-newFieldDiv.appendChild(newControlInputDiv);
-newControlInputDiv.appendChild(newInput);
-newFieldDiv.appendChild(newControlDeleteDiv);
-newControlDeleteDiv.appendChild(newADelete);
-
-newFieldDiv.setAttribute('class', 'field has-addons number-input-group');
-newFieldDiv.setAttribute('id', 'new-input-field');
-newControlInputDiv.setAttribute('class', 'control');
-newInput.setAttribute('id', 'input-3');
-newInput.setAttribute('class', 'input number-input');
-newInput.setAttribute('type', 'number');
-newInput.setAttribute('step', '1');
-newInput.setAttribute('value', '0');
-newControlDeleteDiv.setAttribute('class', 'control');
-newControlDeleteDiv.setAttribute('id', 'remove-button');
-newADelete.setAttribute('class', 'button is-danger delete-number-input');
-
-
-let clicked = addInputButton.addEventListener('click', addInput);
-
-
-if ( !clicked) {
-
-    let removeButton = document.getElementById('remove-button');
-    removeButton.addEventListener('click', removeInput);
-    
-    function removeInput (event) {
-    
-        let removed = document.getElementById('new-input-field');
-        removed.remove();
-    }
-
-    // console.log('true');
-    
+function multiplyNumbers() {
+  // return the product of all the numbers
+  const numberInputs = document.querySelectorAll(".number-input");
+  let totalProduct = 1;
+  for (let i = 0; i < numberInputs.length; i++) {
+    let numberInput = numberInputs[i];
+    totalProduct *= parseInt(numberInput.value);
+  }
+  return totalProduct;
 }
 
-
+function printProduct() {
+  // print the product of all the numbers
+  productResultContainer.innerText = multiplyNumbers();
 }
 
+function realTime(event) {
+  printProduct();
+  printSum();
+}
+
+function printNumberCount() {
+  // get and print the number of input fields
+  const numberOfInputs = numberInputContainer.children.length;
+  // same result as >
+  // const numberOfInputs = document.querySelectorAll('.input-number').length;
+  numberCountContainer.innerText = numberOfInputs;
+}
+
+function addNumberInput() {
+  // add a number input
+//   const existingInputGroup = document.querySelector('.number-input-group');
+//   const newInputGroup = existingInputGroup.cloneNode(true);
+//   const newNumberInput = newInputGroup.querySelector('.number-input');
+//   newNumberInput.value = '0';
+//   numberInputContainer.appendChild(newInputGroup);
+
+  const newInputGroup = document.createElement('div');
+  newInputGroup.classList.add('field', 'has-addons', 'number-input-group');
+  newInputGroup.innerHTML += `
+    <div class="control">
+      <input class="input number-input" type="number" step="1" value="0">
+    </div>
+    <div class="control">
+      <a class="button is-danger delete-number-input">
+        X
+      </a>
+    </div>
+  `;
+  numberInputContainer.appendChild(newInputGroup);
+
+  printNumberCount();
+  realTime();
+}
+
+function removeNumberInput(eventInformation) {
+  // remove a number input
+  console.log(eventInformation.target, eventInformation.target.matches('.delete-number-input'));
+
+  if (eventInformation.target.matches('.delete-number-input') && numberInputContainer.children.length > 2) {
+    const selectedInputGroup = eventInformation.target.closest('.number-input-group');
+    selectedInputGroup.remove();
+
+    printNumberCount();
+    realTime();
+  }
+}
+
+// add event listeners
+sumSubmitBtn.addEventListener('click', printSum);
+productSubmitBtn.addEventListener('click', printProduct);
+addInputBtn.addEventListener('click', addNumberInput);
+numberInputContainer.addEventListener('click', removeNumberInput);
+numberInputContainer.addEventListener('input', realTime);
 
