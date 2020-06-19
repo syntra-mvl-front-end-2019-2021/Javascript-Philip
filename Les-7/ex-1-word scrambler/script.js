c8f9cab40a77330708904ae80862004b9df0bca5
philip franssen <philipfranssen@hotmail.com>
2020-06-19 17:50:15 +0200

    Les-7

--- Les-7/ex-1-word scrambler/script.js
+++ Les-7/ex-1-word scrambler/script.js
@@ -28,7 +28,7 @@ inputText.addEventListener('input', realtime);
 
 
 function countLetters() {
-    // let string = inputText.value;
+    let string = inputText.value;
     let letters = string.split('');
     for ( let letter in letters) {
         letterInput.innerText = letters.length;
@@ -36,7 +36,7 @@ function countLetters() {
 }
 
 function countWords() {
-    // let string = inputText.value;
+    let string = inputText.value;
     let words = string.split(' ');
     for ( let j = 0; j < words.length; j++) {
         wordInput.innerText = words.length;
@@ -45,7 +45,7 @@ function countWords() {
 
 
 function wordScrambler() {
-    // let string = inputText.value;
+    let string = inputText.value;
     let wordArr = string.split(' ');
     let newString = [];
     
@@ -78,3 +78,85 @@ function wordScrambler() {
 // const randomIndex = Math.floor(Math.random() * wordArray.length);
 
 // console.log(wordArray[randomIndex]);
+
+
+// //select textarea
+// let userInput = document.getElementById('user-input');
+// //select btn
+// let submitBtn = document.getElementById('submit-btn');
+// //select result container
+// let resultContainer = document.getElementById('result-container');
+// //# select wordCount
+// let wordCountContainer = document.getElementById('word-count');
+// //# select letterCount
+// let letterCountContainer = document.getElementById('letter-count');
+
+// function getUserInput() {
+//     //return value of userInput
+//     return userInput.value;
+// }
+
+// function textToWordArray(text) {
+//     //return array of words
+//     return text.split(' ');
+// }
+
+// function arrayToText(array) {
+//     return array.join(' ');
+// }
+
+// function getRandomNumber(max) {
+//     //return random number between 0 and max (including 0 and excluding max)
+//     return Math.floor(Math.random() * max);
+// }
+
+// function scrambleArray(oldArray) {
+//     //return scrambled array
+//     //   for (let i = oldArray.length-1; i >=0; i--) {
+//     //     let randomIndex = getRandomNumber(i);
+//     //     let randomValue = oldArray[randomIndex];
+//     //     let arrayValue = oldArray[i];
+//     //     oldArray[i] = randomValue;
+//     //     oldArray[randomIndex] = arrayValue;
+//     //   }
+
+//     const scrambledArray = [];
+//     while (oldArray.length > 0) {
+//         const randomIndex = getRandomNumber(oldArray.length);
+//         const randomValue = oldArray.splice(randomIndex, 1);
+//         scrambleArray.push(randomValue[0]);
+//     }
+
+//     return scrambleArray;
+// }
+
+// function updateWordCount(array) {
+//     //# return word count
+//     wordCountContainer.innerHTML = array.length;
+// }
+
+// function updateLetterCount(text) {
+//     //# return letter count
+//     letterCountContainer.innerHTML = text.length;
+// }
+
+// function scrambleText(text) {
+//     // return scrambled text
+//     let array = textToWordArray(text);
+//     updateWordCount(array);
+//     let scrambled = scrambleArray(array);
+//     return arrayToText(scrambled);
+// }
+
+// function scramble() {
+//     // update textContent of resultContainer
+//     let text = getUserInput();
+//     let scrambledText = scrambleText(text);
+//     resultContainer.innerHTML = scrambledText;
+//     updateLetterCount(text);
+// }
+
+// //add click event listener to submitBtn
+// submitBtn.addEventListener('click', scramble);
+// //## add input event listener to userinput for realTimeScramble
+// userInput.addEventListener('input', scramble);
---
+++ Les-7/ex-2-connect-4/script.js
@@ -0,0 +1,14 @@
+// let column = document.getElementById('col1');
+// console.log(column);
+
+// // wrm gaat getAttribute wel bij getElementById en niet bij getelbyclass
+// let col = column.getAttribute('data-index');
+// console.log(col);
+
+
+let column = document.querySelectorAll('.col');
+console.log(column);
+
+// wrm gaat getAttribute wel bij getElementById en niet bij getelbyclass
+let col = column.querySelectorAll('dataseindex');
+console.log(col);
