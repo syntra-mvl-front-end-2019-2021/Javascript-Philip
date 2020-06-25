66f6700f11070fe7c9d9ae988aeda2bb28f42bec
philip franssen <philipfranssen@hotmail.com>
2020-06-25 13:13:51 +0200

    LES-8 ex

--- Les-8/script.js
+++ Les-8/script.js
@@ -1,146 +1,139 @@
 const randomWords = [
-    'condition',
-    'bottom',
-    'lineage',
-    'trip',
-    'reporter',
-    'paper',
-    'colorful',
-    'agent',
-    'justify',
-    'torture',
-    'cap',
-    'earthflax',
-    'payment',
-    'research',
-    'picture',
-    'garage',
-    'honor',
-    'memorial',
-    'planet',
-    'biography',
-    'profound',
-    'rumor',
-    'gear',
-    'bedroom',
-    'orthodox',
-    'penalty',
-    'grief',
-    'promote',
-    'roof',
-    'suite',
-    'moving',
-    'killer',
-    'museum',
-    'essay',
-    'fever',
-    'dignity',
-    'shadow',
-    'enjoy',
-    'kill',
-    'shy',
-    'counter',
-    'pawn',
-    'button',
-    'bullet',
-    'skin',
-    'rate',
-    'shop',
-    'consider',
-    'other',
-    'prospect',
-  ];
-  let init = document.querySelector('body');
-  const hangManImage = document.querySelector('#image');
-  const solutionContainer = document.querySelector('#solution-container');
-  const winOrLoseContainer = document.querySelector('#win-lose-container');
-  const letterContainer = document.querySelector('#letter-container');
-  const gameState = {
-    word: [],
-    hangman: 1,
-    turn: 1,
-    lettersFound: 0,
-    won: false,
-    lost: false,
-  };
-  
-  // function initGame() {
-  //   // choose a random word > save in gamestate
-  //   // reset gamestate
-  //   // update solutionContainer > insert div.solution-letter per letter
-  //   // empty winOrLoseContainer.innerHTML
-  //   // remove success and failed classes from .letter
-  // }
-  
-  // function clickLetter(event) {
-  //   // check if event.target is .letter
-  //   // check if event.target has success or failed class
-  //   // ---
-  // }
-  
-  // hangManImage.setAttribute('src', 'images/hangman02.png');
+  'condition',
+  'bottom',
+  'lineage',
+  'trip',
+  'reporter',
+  'paper',
+  'colorful',
+  'agent',
+  'justify',
+  'torture',
+  'cap',
+  'earthflax',
+  'payment',
+  'research',
+  'picture',
+  'garage',
+  'honor',
+  'memorial',
+  'planet',
+  'biography',
+  'profound',
+  'rumor',
+  'gear',
+  'bedroom',
+  'orthodox',
+  'penalty',
+  'grief',
+  'promote',
+  'roof',
+  'suite',
+  'moving',
+  'killer',
+  'museum',
+  'essay',
+  'fever',
+  'dignity',
+  'shadow',
+  'enjoy',
+  'kill',
+  'shy',
+  'counter',
+  'pawn',
+  'button',
+  'bullet',
+  'skin',
+  'rate',
+  'shop',
+  'consider',
+  'other',
+  'prospect',
+];
+let init = document.querySelector('body');
+const hangManImage = document.querySelector('#image');
+const solutionContainer = document.querySelector('#solution-container');
+const winOrLoseContainer = document.querySelector('#win-lose-container');
+const letterContainer = document.querySelector('#letter-container');
+let randomWord = randomWords[Math.floor(Math.random()*randomWords.length)].toUpperCase();
+let randomWordArr = randomWord.split('');
+let containerLetters = solutionContainer.children;
+const gameState = {
+  word: [],
+  hangman: 1,
+  turn: 0,
+  lettersFound: 0,
+  won: false,
+  lost: false,
+};
+
+
 
 window.addEventListener('load', initGame)
 
 
-  function initGame() {
-    addLetters();
-    
-    
+function initGame() {
+  addLetters();
+      
+}
+
+
+
+function addLetters() {
+  
+  console.log(randomWord);
+  let letter = document.querySelector('.solution-letter');
+  // console.log(letter);
+  
+  for ( i = 1; i < randomWordArr.length; i++) {
+    let newLetters = letter.cloneNode(true);
+      solutionContainer.appendChild(newLetters);
+
   }
 
-  let randomWord = randomWords[Math.floor(Math.random()*randomWords.length)].toUpperCase();
-  let randomWordArr = randomWord.split('');
-  let containerLetters = solutionContainer.children;
-  
- function addLetters() {
-    // let randomWord = randomWords[Math.floor(Math.random()*randomWords.length)];
-    console.log(randomWord);
-    let letter = document.querySelector('.solution-letter');
-    // console.log(letter);
-    
-    // let randomWordArr = randomWord.split('');
-    // console.log(randomWordArr);
-    for ( i = 1; i < randomWordArr.length; i++) {
-      let newLetters = letter.cloneNode(true);
-        solutionContainer.appendChild(newLetters);
+}
 
-    }
+letterContainer.addEventListener('click', clickLetter);
 
- }
-
- letterContainer.addEventListener('click', clickLetter);
-
- let divLett = document.getElementsByClassName('solution-letter');
+let divLett = document.getElementsByClassName('solution-letter');
 //  console.log(divLett);
 
- function clickLetter(letter) {
-   let clickedLett = letter.target.innerHTML;
-    // console.log(letter.target, letter.target.matches('.letter'));
-    // console.log(clickedLett);
+function clickLetter(letter) {
+ let clickedLett = letter.target.innerHTML;
+ gameState.turn++;
+//  console.log(gameState.turn);
+//  console.log(gameState.hangman);
+ // console.log(randomWordArr);
+  // console.log(randomWordArrLett);
+  
+  
+  
+  for (let j = 0;j < randomWordArr.length;j++) {
+    let randomWordArrLett = randomWordArr[j];
 
-    let randomWordArrLett = randomWord[i];
-    
-    for (randomWordArrLett of randomWord) {
-
-      if( clickedLett === randomWordArrLett) {
-        
-        letter.target.classList.add('success');
-
-        let index = randomWordArr.indexOf(clickedLett);
-        console.log(index);
-        let indexContainer = containerLetters[index];
-        console.log(indexContainer);
-        indexContainer.innerText = clickedLett.value;
-        console.log(solutionContainer);
-        
-      }
-      else {
-        letter.target.classList.add('failed');
+    if( clickedLett === randomWordArrLett) {
       
-      }
-    
+      letter.target.classList.add('success');
+      gameState.lettersFound++;
+      console.log(gameState.lettersFound);
+      let index = randomWordArr.indexOf(clickedLett);
+      // console.log(index);
+      let indexContainer = containerLetters[index];
+      // console.log(indexContainer);
+      indexContainer.innerText = clickedLett.value;
+      // console.log(solutionContainer);
       
-  }
+      
+    }
+    else {
+      letter.target.classList.add('failed');
+      gameState.hangman++;
+      hangManImage.setAttribute('src', 'images/hangman0' + gameState.hangman + '.png')
+    }
+  
+    
+}
+
+
 }
 
