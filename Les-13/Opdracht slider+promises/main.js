a658ce2f9f36354b95fdcde2eaf621ca3a216d53
philip franssen <philipfranssen@hotmail.com>
2020-11-15 18:58:07 +0100

    slider+promises

--- Les-13/Opdracht slider+promises/main.js
+++ Les-13/Opdracht slider+promises/main.js
@@ -1,27 +1,96 @@
-let containerSlider = document.querySelector('.container');
-let imgArr = ['dog1', 'dog2', 'dog3'];
+let imgArr = [
+    'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&auto=format&fit=crop&w=639&q=80',
+    'https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&auto=format&fit=crop&w=612&q=80'
+];
 
-let divImg = document.createElement('div');
-containerSlider.appendChild(divImg);
-divImg.classList.add('div-img');
+console.log(imgArr[0]);
+console.log(imgArr[1]);
+console.log(imgArr[2]);
 
+let container = document.querySelector('.container');
+let slider = document.createElement('div');
+let item = document.createElement('div');
 let img = document.createElement('img');
-divImg.appendChild(img);
-
-
-// img.style.height = '200px';
-img.setAttribute('src', 'dog1.jpg');
-img.classList.add('image');
-
-// function randomImages() {
-//     let randomImg = imgArr[Math.floor(Math.random() * imgArr.length)];
-//     img.setAttribute('src', randomImg + '.jpg');
-
-// }
-
 let prevbtn = document.createElement('button');
 let nextbtn = document.createElement('button');
-divImg.appendChild(prevbtn);
-divImg.appendChild(nextbtn);
-prevbtn.classList.add('prev-btn');
-nextbtn.classList.add('next-btn');
+
+let sliderInfo = {
+    sliderElement: slider,
+    currentSlide: 0,
+};
+
+let x = () => {
+    let arr = [];
+    for (let i = 1; i < imgArr.length; i++) {
+
+        let promise = new Promise(() => {
+            let newImg = item.cloneNode(true);
+            slider.appendChild(newImg);
+            img.setAttribute('src', imgArr[i]);
+            return arr.push(promise);
+        }
+
+        )
+    } console.log(arr);
+}
+
+Promise.all(x).then(() => {
+    createContainer();
+    // for (let i = 1; i < imgArr.length; i++) {
+    //     let newImg = item.cloneNode(true);
+    //     slider.appendChild(newImg);
+    //     img.setAttribute('src', imgArr[i]);
+    // }
+}).catch(() => {
+    console.log("Loading images failed")
+})
+
+function createContainer() {
+    container.appendChild(slider);
+
+    slider.classList.add('slider');
+    slider.appendChild(item);
+
+    item.classList.add('item');
+    item.appendChild(img);
+
+    img.classList.add('image');
+    img.src = imgArr[0];
+
+    container.appendChild(prevbtn);
+    container.appendChild(nextbtn);
+    prevbtn.classList.add('prev-btn');
+    nextbtn.classList.add('next-btn');
+}
+
+function getSliderItemWidth() {
+    return item.clientWidth;
+}
+
+function getShownItems() {
+    return Math.round(slider.clientWidth / getSliderItemWidth());
+}
+
+function nextSlide() {
+    const itemWidth = getSliderItemWidth();
+    sliderInfo.currentSlide++;
+    sliderInfo.sliderElement.style.left =
+        -1 * sliderInfo.currentSlide * itemWidth + 'px';
+
+    // console.log(sliderInfo.currentSlide);
+    // console.log(sliderInfo.sliderElement);
+    // console.log(itemWidth);
+
+}
+
+function previousSlide() {
+    const itemWidth = getSliderItemWidth();
+    sliderInfo.currentSlide--;
+    sliderInfo.sliderElement.style.left =
+        -1 * sliderInfo.currentSlide * itemWidth + 'px';
+
+}
+
+prevbtn.addEventListener('click', previousSlide);
+nextbtn.addEventListener('click', nextSlide);
