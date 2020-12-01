53a41cead8949de07b5dc85d95d32be6acef5eb2
philip franssen <philipfranssen@hotmail.com>
2020-12-01 16:43:54 +0100

    fetch rates

--- Les-14/fetch.js
+++ Les-14/fetch.js
@@ -3,16 +3,32 @@ let secondSelect = document.querySelector('.second-select');
 
 let firstInput = document.querySelector('.input-1');
 let secondInput = document.querySelector('.input-2');
+let date = document.querySelector('.date-input');
 
+// function getDate(date) {
+//     let newDate = new Date();
+//     let chooseDate = newDate.toLocaleDateString('en-CA');
+//     date.value = chooseDate;
+//     return chooseDate;
+// }
+
+function getDate() {
+    let newDate = date.value;
+    // let chooseDate = newDate.toLocaleDateString('en-CA');
+    return newDate;
+    // console.log(newDate);
+}
+console.log(getDate());
 
 function chooseCurr() {
     let baseUrl = 'https://api.exchangeratesapi.io/';
-    let firstCurr = firstSelect.value;
+    // let firstCurr = firstSelect.value;
     let secondCurr = secondSelect.value;
 
-    let rates = fetch(baseUrl + 'latest', {
+    let rates = fetch(baseUrl + getDate(), {
         method: 'GET'
     });
+
 
     rates.then(function (response) {
         console.log(response);
@@ -29,6 +45,7 @@ function chooseCurr() {
         console.log(error);
     })
 }
-chooseCurr()
+chooseCurr();
 // firstSelect.addEventListener('change', chooseCurr);
 secondSelect.addEventListener('change', chooseCurr);
+date.addEventListener('change', getDate);
